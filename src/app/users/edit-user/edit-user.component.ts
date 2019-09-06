bef3ff08b3a0f10aaa96b852d2b66729d5433a9d
Manender Kumar <manender.kumar@mamsys.com>
2019-09-06 14:17:27 +0530

    added roles option in user edit component

--- src/app/users/edit-user/edit-user.component.ts
+++ src/app/users/edit-user/edit-user.component.ts
@@ -6,6 +6,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import Swal from 'sweetalert2';
 import { OrganizationService } from 'src/app/organizations/organization.service';
 import { Organization } from 'src/app/organizations/organization.model';
+import { RoleService } from 'src/app/roles/role.service';
+import { Role } from 'src/app/roles/role.model';
 
 @Component({
   selector: 'app-edit-user',
@@ -13,25 +15,29 @@ import { Organization } from 'src/app/organizations/organization.model';
   styleUrls: ['./edit-user.component.css']
 })
 export class EditUserComponent implements OnInit {
-
   public userId: string;
 
   public organizationOptions: {
-    id: string,
-    name: string
+    id: string;
+    name: string;
+  }[];
+
+  public roleOptions: {
+    id: string;
+    name: string;
   }[];
 
   public editUserForm: FormGroup;
 
   constructor(
     private userService: UserService,
-    private orgService: OrganizationService ,
+    private orgService: OrganizationService,
+    private roleService: RoleService,
     public route: ActivatedRoute,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit() {
-
     this.editUserForm = new FormGroup({
       first_name: new FormControl(null, {
         validators: [Validators.required]
@@ -52,9 +58,8 @@ export class EditUserComponent implements OnInit {
       active: new FormControl()
     });
 
-
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
-      if ( paramMap.has('id') ) {
+      if (paramMap.has('id')) {
         this.userId = paramMap.get('id');
         this.userService.getUser(this.userId).subscribe(response => {
           if (response.status === 'success') {
@@ -64,7 +69,9 @@ export class EditUserComponent implements OnInit {
               last_name: response.user.last_name,
               email: response.user.email,
               employee_id: response.user.employee_id,
-              organization_id: response.user.organization_id ? response.user.organization_id : '',
+              organization_id: response.user.organization_id
+                ? response.user.organization_id
+                : '',
               role_id: response.user.role_id ? response.user.role_id : '',
               dob: new Date(response.user.dob),
               active: response.user.active
@@ -73,10 +80,19 @@ export class EditUserComponent implements OnInit {
         });
 
         this.orgService.getOrganizations(true).subscribe(response => {
-          this.organizationOptions = response.map( (org: Organization) => {
+          this.organizationOptions = response.map((org: Organization) => {
             return {
               id: org.id,
               name: org.name
+            };
+          });
+        });
+
+        this.roleService.getRoles().subscribe(response => {
+          this.roleOptions = response.map((role: Role) => {
+            return {
+              id: role.id,
+              name: role.name
             };
           });
         });
@@ -91,42 +107,31 @@ export class EditUserComponent implements OnInit {
     if (this.editUserForm.invalid) {
       return;
     }
-    this.userService.updateUser(
-      this.userId,
-      this.editUserForm.value.first_name,
-      this.editUserForm.value.middle_name,
-      this.editUserForm.value.last_name,
-      this.editUserForm.value.email,
-      this.editUserForm.value.employee_id,
-      this.editUserForm.value.organization_id,
-      this.editUserForm.value.role_id,
-      this.editUserForm.value.dob,
-      this.editUserForm.value.active === true ? true : false
-    ).subscribe(
-      response => {
-        if (response.status === 'success') {
-          Swal.fire(
-            'Success',
-            'User details are updated',
-            'success'
-          );
-          this.router.navigate(['/users']);
-        } else {
-          Swal.fire(
-            'Error',
-            'User details are not updated',
-            'error'
-          );
+    this.userService
+      .updateUser(
+        this.userId,
+        this.editUserForm.value.first_name,
+        this.editUserForm.value.middle_name,
+        this.editUserForm.value.last_name,
+        this.editUserForm.value.email,
+        this.editUserForm.value.employee_id,
+        this.editUserForm.value.organization_id,
+        this.editUserForm.value.role_id,
+        this.editUserForm.value.dob,
+        this.editUserForm.value.active === true ? true : false
+      )
+      .subscribe(
+        response => {
+          if (response.status === 'success') {
+            Swal.fire('Success', 'User details are updated', 'success');
+            this.router.navigate(['/users']);
+          } else {
+            Swal.fire('Error', 'User details are not updated', 'error');
+          }
+        },
+        error => {
+          Swal.fire('Error', 'User Details are not updated', 'error');
         }
-      },
-      error => {
-        Swal.fire(
-          'Error',
-          'User Details are not updated',
-          'error'
-        );
-      }
-    );
+      );
   }
-
 }
