250d16daede3f44cff7337c14d315b497f3f2366
Manender Kumar <manender.kumar@mamsys.com>
2019-09-06 14:26:24 +0530

    added user organization display in list user component

--- src/app/users/list-users/list-users.component.ts
+++ src/app/users/list-users/list-users.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { User } from '../user.model';
+import { OrganizationService } from 'src/app/organizations/organization.service';
 
 @Component({
   selector: 'app-list-users',
@@ -11,12 +12,21 @@ export class ListUsersComponent implements OnInit {
 
   public users: User[] = [];
   public columnsToDisplay: string[] = ['email', 'first_name', 'last_name', 'organization_id', 'active'];
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private orgService: OrganizationService) { }
 
   ngOnInit() {
     this.userService.getUsers().subscribe(userData => {
       this.users = userData;
+
+      this.users.map((user: User) => {
+        const u: User = user;
+        if (user.organization_id) {
+          this.orgService.getOrganization(user.organization_id).subscribe(response => {
+            u.organization_id = response.name;
+          });
+        }
+        return u;
+      });
     });
   }
-
 }
